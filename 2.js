4c44605510a63799cb0a3d196d07343dd3c52f42
YasirAmmar55 <yasirgch202205>
2026-04-16 06:51:15 +0500

    varibales

--- 2.js
+++ 2.js
@@ -1,1 +1,11 @@
-console.log("yasir")
+const accountId = 1234567890 // accountId is a constant and cannot be changed
+let accountEmail = "yasir@gmail.com" // accountEmail can be changed but accountId cannot be changed
+var accountPassword = "12345678" // Not Used due to scope issue
+accountCity = "Karachi" // it can be used without declaration but it is not recommended
+
+
+// accountId=2
+ console.log(accountId); // Output: TypeError: Assignment to constant variable.
+ 
+console.log(accountEmail); // Output:
+console.table([ accountId, accountEmail, accountCity ])
