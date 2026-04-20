17e6b0121772b4eb936a549de37cfeab0d9038fe
YasirAmmar55 <yasirgch202205>
2026-04-20 10:53:25 +0500

    Comparsion

---
+++ 6.js
@@ -0,0 +1,39 @@
+/*--------------------Numbers----------------*/
+console.log(2>1);
+console.log(2<=1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2!==1);
+
+/*--------------------String with Numbers----------------*/
+console.log("2">1);  // convert  string into number
+console.log("02">=1);
+console.log(02>="1");
+
+/*--------------------Null----------------*/
+console.log(null>0); // comparision >,<,>=,<= convert null to numbers 
+console.log(null==0); // Treats as Null 
+console.log(null>=0); 
+
+/*--------------------Undefined----------------*/
+const undefined
+console.log(undefined>0);  // output; false 
+console.log(undefined==0);  
+console.log(undefined>=0); 
+
+/*--------------------Equality----------------*/
+console.log("2"==1); 
+console.log("1"==1); 
+console.log("2"===1);  // Strict check data type 
+console.log("1"===1);  // Strict check data type 
+
+
+
+
+
+
+
+
+
+
+
