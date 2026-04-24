07979ad4371c4a9b3ac84ac4b8ae3d823ab622f9
YasirAmmar55 <yasirgch202205>
2026-04-24 14:38:53 +0500

    Math

---
+++ 11.js
@@ -0,0 +1,43 @@
+const number = -3.565
+
+console.log(Math.abs(number)); // change sign only "-" -> "+" not positive to negative
+
+console.log(Math.Math.sign(-4)); // If x is positive it returns 1
+                                 //If x is negative it returns -1
+                                 //If x is zero, it returns 0
+
+
+console.log(Math.round(number)); // round number
+
+console.log(Math.ceil(number)); // upper value
+
+console.log(Math.floor(number)); //  first value  number
+
+console.log(Math.min(number)); //  min value in array
+
+console.log(Math.max(number)); //  max value in array
+
+console.log(Math.random());  // radom value between 0 and 1
+
+
+console.log(Math.random() * 10); // multiply by 10, in this case 0 may appear in output
+
+console.log((Math.random() * 10) + 1); // multiply by 10 and add 1, so 0 will not occur
+
+console.log(Math.floor(Math.random() * 10) + 1); // Give Only One Random number
+
+
+
+
+
+// Math.trunc(x)
+//Math.E        // returns Euler's number
+// Math.PI       // returns PI
+// Math.SQRT2    // returns the square root of 2
+// Math.SQRT1_2  // returns the square root of 1/2
+// Math.LN2      // returns the natural logarithm of 2
+// Math.LN10     // returns the natural logarithm of 10
+// Math.LOG2E    // returns base 2 logarithm of E
+// Math.LOG10E   // returns base 10 logarithm of E
+
+
